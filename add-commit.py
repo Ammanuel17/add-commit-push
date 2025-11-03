c3a21adafedaa1f60d1f16eb42d7c7fd906369d3
Amanuale Yimenu <amanualeyimenu@Amanuales-MacBook-Air.local>
2025-11-02 22:54:55 -0600

    "update files"

--- add-commit.py
+++ add-commit.py
@@ -1,13 +1,14 @@
 import subprocess
-print ("sttarting  add-commit-push")
+print ("starting add-commit-push")
 print ("git status")
+subprocess.run(["git", "status"])
 
-subprocess.run(["git", "status", "."])
 print ("git add -A")
+subprocess.run(["git", "add", "-A"])
 
-subprocess.run(["git", "commit", "-m", "Automated commit"])
 print ("git commit -m" )
-
 subprocess.run(["git", "commit", "-m", "\"update files\""])
+
 print ("git push")
 subprocess.run(["git", "push"])
+
